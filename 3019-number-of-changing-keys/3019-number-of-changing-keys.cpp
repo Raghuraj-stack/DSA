50216f3e1adf041611d946bb01cc5625bc563d08
Raghuraj-stack <studyweb111@gmail.com>
2026-05-09 11:42:51 +0530

    Time: 0 ms (100%), Space: 9 MB (81.87%) - LeetHub

---
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+        s[i] = tolower(s[i]);
+        }
+        int count = 0;
+        for(int i = 0 ; i < s.length() - 1 ; i++)
+        {
+            if(s[i] != s[i+1])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
